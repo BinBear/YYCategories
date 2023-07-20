12f0d33aed511a250f1bf4dee566737c7135ff93
vin <>
2023-07-20 19:45:44 +0800

    💄 修改 numberWithString

--- YYCategories/Foundation/NSNumber+YYAdd.m
+++ YYCategories/Foundation/NSNumber+YYAdd.m
@@ -19,45 +19,21 @@ YYSYNTH_DUMMY_CLASS(NSNumber_YYAdd)
 @implementation NSNumber (YYAdd)
 
 + (NSNumber *)numberWithString:(NSString *)string {
-    NSString *str = [[string stringByTrim] lowercaseString];
-    if (!str || !str.length) {
+    if (![string isKindOfClass:NSString.class] || !string.length) {
         return nil;
     }
-    
-    static NSDictionary *dic;
+    NSString *str = [[string stringByTrim] lowercaseString];
+    static NSNumberFormatter *yy_formatter;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        dic = @{@"true" :   @(YES),
-                @"yes" :    @(YES),
-                @"false" :  @(NO),
-                @"no" :     @(NO),
-                @"nil" :    [NSNull null],
-                @"null" :   [NSNull null],
-                @"<null>" : [NSNull null]};
+        yy_formatter = [NSNumberFormatter new];
+        yy_formatter.usesSignificantDigits = true;
+        yy_formatter.maximumSignificantDigits = 100;
+        yy_formatter.groupingSeparator = @"";
+        yy_formatter.decimalSeparator = @".";
+        yy_formatter.numberStyle = NSNumberFormatterNoStyle;
     });
-    NSNumber *num = dic[str];
-    if (num != nil) {
-        if (num == (id)[NSNull null]) return nil;
-        return num;
-    }
-    
-    // hex number
-    int sign = 0;
-    if ([str hasPrefix:@"0x"]) sign = 1;
-    else if ([str hasPrefix:@"-0x"]) sign = -1;
-    if (sign != 0) {
-        NSScanner *scan = [NSScanner scannerWithString:str];
-        unsigned num = -1;
-        BOOL suc = [scan scanHexInt:&num];
-        if (suc)
-            return [NSNumber numberWithLong:((long)num * sign)];
-        else
-            return nil;
-    }
-    // normal number
-    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
-    return [formatter numberFromString:string];
+    return [yy_formatter numberFromString:str];
 }
 
 @end
